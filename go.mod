ea9ef77291bf265fbffd39b7c1cf90f18a42ec73
tafhdytllah <taufikhh.97@gmail.com>
2024-01-31 13:44:39 +0700

    major change : update say_hello add param,  and go.mod modul name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/tafhero/go-say-hello
+module github.com/tafhero/go-say-hello/v2
 
 go 1.21.6
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello versi terbaru"
+func SayHello(name string) string {
+	return "Hello versi terbaru" + name
 }
